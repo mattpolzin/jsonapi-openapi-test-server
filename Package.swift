e27d26094105965bd89f0da6bf382d5abc089348
Mathew Polzin <matt.polzin@gmail.com>
2020-03-07 18:28:10 -0800

    Update Vapor and OpenAPIKit

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@ import PackageDescription
 let package = Package(
     name: "jsonapi-openapi-test-server",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "jsonapi-openapi-test-server", targets: ["App"]),
         .library(name: "jsonapi-openapi-test-lib", targets: ["APITesting"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3.24"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-beta.3"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta.2.4"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.3"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.1"),
 
-        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", .upToNextMinor(from: "0.2.0")),
-        .package(url: "https://github.com/mattpolzin/VaporOpenAPI.git", .branch("master")),
+        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/mattpolzin/VaporOpenAPI.git", .upToNextMinor(from: "0.0.1")),
 
         .package(url: "https://github.com/weichsel/ZIPFoundation/", .upToNextMinor(from: "0.9.10")),
         .package(url: "https://github.com/ianpartridge/swift-backtrace.git", from: "1.1.1"),
 
         .package(url: "https://github.com/mattpolzin/JSONAPI-OpenAPI.git", .branch("feature/gen-swift")),
-        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .upToNextMinor(from: "0.20.0")),
+        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .upToNextMinor(from: "0.23.0")),
         .package(url: "https://github.com/mattpolzin/JSONAPI.git", .upToNextMajor(from: "3.0.0"))
     ],
     targets: [
