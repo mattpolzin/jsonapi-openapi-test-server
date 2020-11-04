99fe9cf7d98083fc23db092ee032068c4f2e05fd
Mathew Polzin <matt.polzin@gmail.com>
2020-11-04 11:04:02 -0800

    update dependencies and turn an additional OpenAPI validation on.

--- Sources/APITesting/APITestCommand.swift
+++ Sources/APITesting/APITestCommand.swift
@@ -530,6 +530,7 @@ public func produceValidationErrors(
         .validating(.documentContainsPaths)
         .validating(.pathsContainOperations)
         .validating(.schemaComponentsAreDefined)
+        .validating(.pathParametersAreDefined)
 
     do {
         try document
